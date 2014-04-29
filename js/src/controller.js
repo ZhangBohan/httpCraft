5f42de69afcdcd980deaa1164731de1a37dc7998
ZhangBohan <imzhangbohan@gmail.com>
2014-04-29 15:07:01 +0800

    get header and status in page

--- js/src/controller.js
+++ js/src/controller.js
@@ -1,15 +1,20 @@
-function APICtrl($scope, $http) {
-    $scope.url = 'http://chassis.appchina.com:8080/chassis/safety/result' +
-        '?md5=888b80cedbac7cf2bb734bdeb70065a0&callback=JSON_CALLBACK';
+var apiTalk = angular.module('apiTalk', ['ui.bootstrap']);
+
+apiTalk.controller('APICtrl', function($scope, $http) {
+    $scope.url = 'http://guides.appchina.com/guide/apps/4';
 
     $scope.getUrl = function() {
         console.debug('url:', $scope.url);
-        $http.get($scope.url).success(function(data, status, headers, config) {
+        $http({method: 'GET', url: $scope.url, config: {
+
+        }}).success(function(data, status, headers, config) {
             console.log('data:', data);
             console.log('status:', status);
-            console.log('headers:', headers);
+            console.log('headers:', headers());
             console.log('config:', config);
             $scope.result = data;
+            $scope.headers = headers();
+            $scope.status = status;
         })
     }
-}
+});
