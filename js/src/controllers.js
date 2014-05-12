6e9a5e3f355f88242273862761c73d037815c3da
ZhangBohan <imzhangbohan@gmail.com>
2014-05-12 10:54:26 +0800

    change url don't init request

--- js/src/controllers.js
+++ js/src/controllers.js
@@ -124,11 +124,6 @@ httpCraftControllers.controller('APICtrl', ['$scope', '$http', 'HttpUtils',
             return tips;
         };
 
-        $scope.urlSelected = function(url) {
-            $scope.currentRequest = RequestStorage.defaultRequest;
-            $scope.currentRequest.url = url;
-        };
-
         var notRepeat = function (array, value) {
             for(var i = 0; i < array.length; i++) {
                 if(array[i] == value) {
